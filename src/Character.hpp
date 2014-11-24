aecb7ed9aa8b7b85b2766181d050bf779d723aeb
Darthcaboose <sanifakhouri@gmail.com>
2014-11-24 00:44:16 -0800

    Attributes Addition

---
+++ src/Character.hpp
@@ -0,0 +1,51 @@
+/*
+ * Character.hpp
+ * Character class, base class for Hero, enemies, so on
+ *
+ *  Created on: Nov 17, 2014
+ *      Author: Tony
+ */
+
+#ifndef SRC_CHARACTER_HPP_
+#define SRC_CHARACTER_HPP_
+
+#include <utility>
+#include <string>
+
+class Character
+{
+private:
+	unsigned int HitPoints;
+	unsigned int HitPointsMax;
+	unsigned int ShieldPoints;
+	unsigned int ShieldPointsMax;
+	float MovementSpeed;
+	unsigned int AttackDamage;
+	unsigned int AttackSpeed;
+	unsigned int Armor;
+	float InvulnerablityTime;
+
+	float AbilityCooldown1Current;
+	float AbilityCooldown1Max;
+	float AbilityCooldown2Current;
+	float AbilityCooldown2Max;
+	float AbilityCooldown3Current;
+	float AbilityCooldown3Max;
+	float AbilityCooldown4Current;
+	float AbilityCooldown4Max;
+
+public:
+	std::pair<int, int> Coordinates;	// Stores current position (X, Y) of the character
+
+	int getX(){
+		return Coordinates.first;
+	}
+
+	int getY(){
+		return Coordinates.second;
+	}
+};
+
+
+
+#endif /* SRC_CHARACTER_HPP_ */
